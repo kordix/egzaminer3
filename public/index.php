82c4e76e1a3ee57b84dbf0931fa6a495f7025fab
Kordian Bober <BOBKOR@ad.bertrand.pl>
2024-07-07 21:40:38 +0200

    favicon

--- public/index.php
+++ public/index.php
@@ -17,6 +17,8 @@ if(!isset($_SESSION['zalogowany'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Egzaminer 3</title>
 
+    <link rel="icon" href="data:;base64,=">
+
 
     <link rel="stylesheet" href="css/mybootstrap.css">
     <link rel="stylesheet" href="css/style.css">
