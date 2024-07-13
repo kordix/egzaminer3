5e9a235af060fcfba6d23287c9c1e40dd68c12cc
Kordian Bober <BOBKOR@ad.bertrand.pl>
2024-07-13 22:08:12 +0200

    updated at

--- public/api/questions.php
+++ public/api/questions.php
@@ -4,8 +4,8 @@ session_start();
 
 require('db.php');
 
-$query = "select q.*,ifnull(r.counter,0) as counter , ifnull(r.updated_at,'') from questions q
-left outer join results r on r.question_id = q.id and r.user_id = ? order by id desc
+$query = "select q.id,q.question,q.answer,q.language,q.partofspeech, q.tags, q.rodzajnik, q.fav, q.level, q.counter, q.sentence, ifnull(r.counter,0) as counter , ifnull(r.updated_at,'') as updated_at from questions q
+left outer join results r on r.question_id = q.id and r.user_id = ? order by q.id desc limit 10
 ";
 
 $sth = $dbh->prepare($query);
