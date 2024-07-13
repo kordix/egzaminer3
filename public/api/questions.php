3399301ef9623bf2071832499199085adb92efbd
Kordian Bober <BOBKOR@ad.bertrand.pl>
2024-07-13 21:58:43 +0200

    sortbug

--- public/api/questions.php
+++ public/api/questions.php
@@ -4,7 +4,7 @@ session_start();
 
 require('db.php');
 
-$query = "select q.*,ifnull(r.counter,0) as counter , r.updated_at from questions q
+$query = "select q.*,ifnull(r.counter,0) as counter , ifnull(r.updated_at,'') from questions q
 left outer join results r on r.question_id = q.id and r.user_id = ? order by id desc
 ";
 
