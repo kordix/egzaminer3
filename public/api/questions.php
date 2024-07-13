d57b44ff25585548390d2b6873324cedd790a4ed
Kordian Bober <BOBKOR@ad.bertrand.pl>
2024-07-13 22:12:21 +0200

    order counter

--- public/api/questions.php
+++ public/api/questions.php
@@ -5,7 +5,7 @@ session_start();
 require('db.php');
 
 $query = "select q.id,q.question,q.answer,q.language,q.partofspeech, q.tags, q.rodzajnik, q.fav, q.level, q.counter, q.sentence, ifnull(r.counter,0) as counter , ifnull(r.updated_at,'') as updated_at from questions q
-left outer join results r on r.question_id = q.id and r.user_id = ? order by q.id desc
+left outer join results r on r.question_id = q.id and r.user_id = ? order by q.counter, q.id desc
 ";
 
 $sth = $dbh->prepare($query);
